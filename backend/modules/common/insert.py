3cb31edf3456221dba8a9de3a5bfdfc5c1ba0378
Kgiruc <kgiruc76@gmail.com>
2025-07-11 20:18:30 +0200

    changes to common models

--- backend/modules/common/insert.py
+++ backend/modules/common/insert.py
@@ -1,22 +1,25 @@
 from backend.db.database import get_connection
 from backend.utils.logger import logger
 
-def insert_temperature_data(sensor_id, value, timestamp):
+def insert_sensor_data(sensor_type: str, sensor_id: int, data: dict):
+    table_name = f"{sensor_type}_data"
+    value = data.get("value")
+    timestamp = data.get("timestamp")
+
     try:
         conn = get_connection()
         cursor = conn.cursor()
 
-        query = """
-        INSERT INTO temperature_data (sensor_id,value, timestamp)
-        VALUES (%s,%s,%s)
+        query = f"""
+        INSERT INTO {table_name} (sensor_id, value, timestamp)
+        VALUES (%s, %s, %s)
         """
-        cursor.execute(query,(sensor_id, value, timestamp))
+        cursor.execute(query, (sensor_id, value, timestamp))
         conn.commit()
 
-        logger.info("dane zapisane")
+        logger.info(f"[{sensor_type}] Dane zapisane w tabeli {table_name}")
     except Exception as e:
-        logger.error(f"bład: {e}")
-        return
+        logger.error(f"[{sensor_type}] Błąd insertu: {e}")
     finally:
         if cursor:
             cursor.close()
