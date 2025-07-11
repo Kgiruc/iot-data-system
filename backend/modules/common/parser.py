a69578d15100ee0bb500c6d6f21c757c3a3d6eb0
Kgiruc <kgiruc76@gmail.com>
2025-07-11 23:56:53 +0200

    fixes code to common logic

--- backend/modules/common/parser.py
+++ backend/modules/common/parser.py
@@ -1,12 +1,17 @@
 import json
 from datetime import datetime, timezone
 from backend.utils.logger import logger
+from backend.validators.temperature_validator import validate_temperature
 
 def parse_common_fields(payload: str) -> dict | None:
     try:
         data = json.loads(payload)
         if not all(k in data for k in ("sensor_name", "value")):
             raise ValueError("Brak wymaganych pól")
+        
+        if not validate_temperature(data):
+            raise ValueError("Walidacja nie powiodła się")
+
 
         data["timestamp"] = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
         return data
