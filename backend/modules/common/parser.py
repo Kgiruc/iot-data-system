55bd0f6c86c1ae6304ba252f006a19e312e47201
Kgiruc <kgiruc76@gmail.com>
2025-07-11 20:14:27 +0200

    changes to common models

--- backend/modules/common/parser.py
+++ backend/modules/common/parser.py
@@ -2,15 +2,15 @@ import json
 from datetime import datetime, timezone
 from backend.utils.logger import logger
 
-
-def parse_payload(payload: str) -> dict | None:
+def parse_common_fields(payload: str) -> dict | None:
     try:
         data = json.loads(payload)
-        if not all(k in data for k in("sensor_name","value")):
-            raise ValueError("brak wymaganych pól")
-        
+        if not all(k in data for k in ("sensor_name", "value")):
+            raise ValueError("Brak wymaganych pól")
+
         data["timestamp"] = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
         return data
+
     except Exception as e:
-        logger.error(f"bład parsownia:{e}")
+        logger.error(f"Błąd parsowania: {e}")
         return None
